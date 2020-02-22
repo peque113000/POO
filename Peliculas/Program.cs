4fc0a0892dbd51a42c5bdc623dcbab153f714c0f
peque113000 <60371534+peque113000@users.noreply.github.com>
2020-02-21 20:43:10 -0800

    Publico

---
+++ Peliculas/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Peliculas
+{
+    class Peliculas
+    {
+        public string Titulo;
+        public Int16 año;
+        public string Pais;
+        public string Director;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Peliculas pel= new Peliculas();
+
+
+            pel.Titulo = "De aqui a la eternidad";
+            pel.año = 1953;
+            pel.Pais = "Estados Unidos";
+            pel.Director = "Fred Zinnemann";
+            Console.WriteLine("Peliculas: {0}, año{1}, Pais{2}, Director{3}", pel.Titulo, pel.año, pel.Pais, pel.Director);
+        }
+    }
+}
