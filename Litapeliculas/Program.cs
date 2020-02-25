15bd49b1e2efbb3976f3aa519306adcee549db13
peque113000 <60371534+peque113000@users.noreply.github.com>
2020-02-24 20:43:18 -0800

    Listapeliculas

---
+++ Litapeliculas/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListaPeliculas
+{
+    class Peli
+    {
+        public string nombre;
+        
+        public void imprime(){
+            Console.WriteLine("({0})",nombre);
+        }
+
+        public Pelis(string p){
+            nombre=p;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Peli> Pelis= new List<Peli>();
+            Pelis.Add(new Peli("De aqui a la eternidad"));
+            Pelis.Add(new Peli("Guason"));
+            Pelis.Add(new Peli("Cadena de favores"));
+            Pelis.Add(new Peli("Toy Stori 4"));
+            Pelis.Add(new Peli("Judy"));
+            
+            foreach(Peli p in pelis)
+            p.imprime();
+        }
+    }
+}
